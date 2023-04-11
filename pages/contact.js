772bff77b3031d01003035ff65307d62fc529e22
vishu bansal <bansalvishu56@gmail.com>
2023-04-12 02:00:41 +0530

    Added animation

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import { motion as m } from "framer-motion"
 
 export default function Contact(){
     return(
-        <main>
+        <m.main initial={{y: "100%"}} animate={{y: "0%"}} transition={{duration: 0.75, ease: "easeOut"}} exit={{opacity:1}} className="text--gray-900 absolute top-0 left-0 w-full h-full bg-red-400 lg:px-46 px-16" >
             <div className="my-96 p-1">
                 <h1 className="text-6xl text-center lg:text-right lg:text-9xl">Let{"'"}s Talk</h1>
             </div>
@@ -19,6 +19,6 @@ export default function Contact(){
                     </ul>
                 </div>
             </div>
-        </main>
+        </m.main>
     );
 }
